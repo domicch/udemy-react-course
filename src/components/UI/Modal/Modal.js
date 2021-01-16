6bec11dddd3ad361c787d3aeb82e235d16be3825
domicch <domicch@gmail.com>
2021-01-16 01:08:54 +0000

    performance tuning

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop 
-            show={props.show} 
-            clicked={props.modalClosed}
-        />
-        <div 
-            className={classes.Modal}
-            style={
-                {
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }
-            }
-        >
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component{
+    // performance tuning - don't render unless modal is visible, or switching visibility
+    shouldComponentUpdate(nextProps, nextState){
+        if(this.props.show || this.props.show !== nextProps.show){
+            return true;
+        }
+        return false;
+    }
 
-export default modal;
+    render() {
+        return (
+            <Aux>
+                <Backdrop 
+                    show={this.props.show} 
+                    clicked={this.props.modalClosed}
+                />
+                <div 
+                    className={classes.Modal}
+                    style={
+                        {
+                            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                            opacity: this.props.show ? '1' : '0'
+                        }
+                    }
+                >
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+}
+
+export default Modal;
